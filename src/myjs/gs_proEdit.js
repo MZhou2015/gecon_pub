ca06545a067d355b350b5c5ea197b593aad69394
Michael <ming01zhou@gmail.com>
2020-10-27 18:01:01 -0700

    gs_proEdit

---
+++ src/myjs/gs_proEdit.js
@@ -0,0 +1,38 @@
+/*****************************************************************************************/
+/****      Component  product Edit   October 27 , 2020     by Michael zhou            *****/
+/******************************************************************************************/
+var  proEdit = Vue.component('proEdit' , 
+ { 
+  props: ['prod','sum'] ,
+  template:  ` <div class="product" >
+  </div> `  ,
+  data() {
+      return {  
+            checked: false,
+            records:'',
+            pinfo:'',
+            tot:{} 
+               }
+ },
+mounted: function(){
+    let passdata = this.pinfo ;
+   this.postfor_update(passdata)
+},
+methods:{
+    postfor_update: function (passdata){
+        let url = './src/php/db_write.php'; 
+        axios.post(url, JSON.stringify(passdata)).then(function (response) {
+                        console.log(response.data);
+                      }).catch(function (error) {
+                        console.log(error);
+                            });
+    this.tm = false ; 
+    this.stat1 = "New Transation completed !" ;
+    console.log(mn) ;
+         // const words = mn.split('-');
+         // mn = words[0] ;
+      } 
+   
+     }
+})
+/**********************************************
