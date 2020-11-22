d5b3e381559498cc0fa55d03092df2e5516e10f0
Michael <ming01zhou@gmail.com>
2020-11-21 22:11:38 -0800

    vue

---
+++ myVueproject/src/components/catalog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="catalog">
+   New router
+   <i class="fa fa-phone-alt"></i> &nbsp; 604 879-6399 &nbsp; &nbsp;
+    <router-link to="/" class="btn btn-primary"> Home</router-link>
+    <router-link to="/Product" class="btn btn-success"> Product</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Catalog'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  font-weight: normal;
+  font-size: 14px;
+}
+</style>
