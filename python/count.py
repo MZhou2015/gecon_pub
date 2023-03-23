2ac1a11518f31b3ea71e57eae82a7005adbabf56
Michael <ming01zhou@gmail.com>
2023-03-22 22:21:36 -0700

    creditcard

--- python/count.py
+++ python/count.py
@@ -14,3 +14,30 @@ str1="hello World"
 sl = len(text) 
 print(sl)
 print(text)
+
+import openpyxl
+# 创建一个新的工作簿
+workbook = openpyxl.Workbook()
+
+# 选择默认的工作表
+sheet = workbook.active
+
+# 将B3和B4单元格分别赋值为45和69
+sheet['B3'] = 45
+sheet['B4'] = 69
+
+# 保存工作簿
+workbook.save('d:/save/test.xlsx')
+# 打开工作簿
+workbook = openpyxl.load_workbook('d:/save/test.xlsx')
+
+# 选择默认的工作表
+sheet = workbook.active
+
+# 从B3和B4单元格分别获取值
+a = sheet['B3'].value
+b = sheet['B4'].value
+
+# 计算a*b并打印结果
+result = a * b
+print(result)
